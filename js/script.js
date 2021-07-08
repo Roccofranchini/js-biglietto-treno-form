d921fd9a9205fbaffb33fd423d81293e98cebf4b
Roccofranchini <rocco.franchini1@gmail.com>
2021-07-08 16:51:45 +0200

    riferimenti-add

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*
+
+1- creare i riferimenti agli input del form
+2- creo un bottone il cui click darà il via alle operazioni
+3- raccolgo i dati dell'utente
+4- calcolo il prezzo ed eventuali sconti
+5- stampiamo
+
+
+*/
+
+
+//Riferimenti FORM
+
+var formName = document.getElementById('formname');
+var formKms = document.getElementById('formkms');
+var formAge = document.getElementById('formage');
+var buttonSubmit = document - getElementById('submit');
+
+//Riferimenti Ticket
+
+var passengerName = document.getElementById('passenger-name');
+var passengerTrain = document.getElementById('passenger-train');
+var passengerCar = document.getElementById('passenger-car');
+var passengerPrice = document.getElementById('passenger-price');
+var passengerDiscount = document.getElementById('passenger-discount');
